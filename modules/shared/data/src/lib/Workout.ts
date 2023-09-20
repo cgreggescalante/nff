b4423eb7f9ea740b19532602bab710c633318ded
Conor <conor@johngregg.org>
2023-09-20 14:59:41 -0500

    NFF-5 Remote Database - Workout as class

--- modules/shared/data/src/lib/Workout.ts
+++ modules/shared/data/src/lib/Workout.ts
@@ -1,24 +1,30 @@
 import { WorkoutType, WorkoutTypeFromName } from "./WorkoutType";
 import { FirestoreDataConverter } from "firebase/firestore";
 
-export interface Workout {
-  workoutType: WorkoutType,
-  duration: number,
-  points: number
-}
+export class Workout {
+  workoutType: WorkoutType;
+  duration: number;
+  points: number;
 
-export const workoutConverter: FirestoreDataConverter<Workout> = {
-  toFirestore: (workout: Workout) => ({
-    workoutType: workout.workoutType.name,
-    duration: workout.duration,
-    points: workout.points
-  }),
-  fromFirestore: (snapshot, options): Workout => {
-    const data = snapshot.data(options);
-    return {
-      workoutType: WorkoutTypeFromName(data['workoutType']),
-      duration: data['duration'],
-      points: data['points'],
-    };
+  constructor(workoutType: WorkoutType, duration: number, points: number) {
+    this.workoutType = workoutType;
+    this.duration = duration;
+    this.points = points;
+  }
+
+  static converter: FirestoreDataConverter<Workout> = {
+    toFirestore: (workout: Workout) => ({
+      workoutType: workout.workoutType.name,
+      duration: workout.duration,
+      points: workout.points
+    }),
+    fromFirestore: (snapshot, options): Workout => {
+      const data = snapshot.data(options);
+      return {
+        workoutType: WorkoutTypeFromName(data['workoutType']),
+        duration: data['duration'],
+        points: data['points'],
+      };
+    }
   }
 }
