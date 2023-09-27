685fb96083a6cd1c79e78b2c237b01fabf0cd4c2
Conor <conor@johngregg.org>
2023-09-27 16:49:22 -0500

    Removed Center

--- modules/shared/ui/src/index.ts
+++ modules/shared/ui/src/index.ts
@@ -1,6 +1,5 @@
 export * from './lib/footer/footer'
 export * from './lib/header/header'
-export * from './lib/center/center'
 export * from './lib/upload-list/upload-list'
 export * from './lib/workout-input/workout-input'
 export * from './lib/edit-user-details/edit-user-details'
