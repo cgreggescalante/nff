8b1c03e4847bf21a80e86e06c49bc59f3eeb55e7
Conor <conor@johngregg.org>
2023-12-06 13:59:44 -0600

    NFF-16 Refactor Data Model - Simplified UploadConverter

--- modules/shared/data/src/lib/converters/UploadConverter.ts
+++ modules/shared/data/src/lib/converters/UploadConverter.ts
@@ -1,23 +1,17 @@
-import { DocumentReference, FirestoreDataConverter } from '@firebase/firestore';
+import {
+  FirestoreDataConverter,
+  QueryDocumentSnapshot,
+} from '@firebase/firestore';
 import type Upload from '../models/Upload';
 import type { UploadWithMetaData } from '../models/Upload';
+import { withMetaData } from '../services/read/all';
 
 const UploadConverter: FirestoreDataConverter<Upload> = {
   toFirestore: (upload: Upload) => Object.assign({}, upload),
-  fromFirestore: (snapshot, options): UploadWithMetaData => {
-    const data = snapshot.data(options);
-
-    return {
-      userRef: data['user'],
-      userFirstName: data['userFirstName'],
-      userLastName: data['userLastName'],
-      description: data['description'],
-      date: new Date(data['date']['seconds'] * 1000),
-      workouts: data['workouts'],
-      uid: snapshot.id,
-      ref: snapshot.ref as DocumentReference<Upload>,
-    };
-  },
+  fromFirestore: (snapshot, options): UploadWithMetaData => ({
+    ...withMetaData(snapshot as QueryDocumentSnapshot<Upload>, options),
+    date: new Date(snapshot.data(options)['date']['seconds'] * 1000),
+  }),
 };
 
 export default UploadConverter;
--- modules/shared/data/src/lib/models/Upload.ts
+++ modules/shared/data/src/lib/models/Upload.ts
@@ -2,9 +2,10 @@ import { DocumentReference } from '@firebase/firestore';
 
 import { WorkoutTypeToNumber } from './WorkoutType';
 import { WithMetaData } from './Models';
+import UserInfo from './UserInfo';
 
 export interface Upload {
-  userRef: DocumentReference;
+  userRef: DocumentReference<UserInfo>;
   userFirstName: string;
   userLastName: string;
   description: string;
