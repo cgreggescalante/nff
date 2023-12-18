fe362e0f265d29018d0b6f65be873d21fedf345f
Conor <conor@johngregg.org>
2023-12-18 10:58:08 -0600

    Header is properly aligned, not collapsible

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -22,47 +22,46 @@ export const Header = () => {
   return (
     <Navbar expand="md" fixed="top" className="bg-body-secondary">
       <Container>
-        <Navbar.Brand as={Link} to="/">
-          NFF
-        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Navbar.Brand as={Link} to="/">
+            NFF
+          </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Nav.Link as={Link} to="/events">
+            Events
+          </Nav.Link>
 
-        <Navbar.Collapse>
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/events">
-              Events
-            </Nav.Link>
-          </Nav>
-
-          <Nav className="ml-auto">
-            {auth.currentUser == null ? (
-              <>
-                <Nav.Link as={Link} to="/login">
-                  Login
-                </Nav.Link>
-                <Nav.Link as={Link} to="/signup">
-                  Sign Up
-                </Nav.Link>
-              </>
-            ) : (
-              <>
-                <Nav.Link as={Link} to="/user-dashboard">
-                  Dashboard
-                </Nav.Link>
-                <Nav.Link as={Link} to="/upload">
-                  Upload
-                </Nav.Link>
-                <Nav.Link onClick={handleSignOut}>Sign Out</Nav.Link>
-              </>
-            )}
-            {isAdmin && (
-              <Nav.Link as={Link} to={'/addUser-tools'}>
-                Admin
+          {auth.currentUser && (
+            <>
+              <Nav.Link as={Link} to="/user-dashboard">
+                Dashboard
               </Nav.Link>
-            )}
-          </Nav>
-        </Navbar.Collapse>
+              <Nav.Link as={Link} to="/upload">
+                Upload
+              </Nav.Link>
+            </>
+          )}
+        </Nav>
+
+        <Nav className="ml-auto">
+          {auth.currentUser == null ? (
+            <>
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
+              <Nav.Link as={Link} to="/signup">
+                Sign Up
+              </Nav.Link>
+            </>
+          ) : (
+            <Nav.Link onClick={handleSignOut}>Sign Out</Nav.Link>
+          )}
+          {isAdmin && (
+            <Nav.Link as={Link} to={'/addUser-tools'}>
+              Admin
+            </Nav.Link>
+          )}
+        </Nav>
       </Container>
     </Navbar>
   );
