5c168b0bf0c603a4f9c9687aafedb082101b4317
Conor <conor@johngregg.org>
2023-12-22 23:04:24 -0600

    NFF-19 Styling - UploadList with material ui

--- src/app/components/upload-list/upload-card/upload-card.tsx
+++ src/app/components/upload-list/upload-card/upload-card.tsx
@@ -1,38 +1,39 @@
-import { Card, Table } from 'react-bootstrap';
+import { Card, Table, Typography } from '@mui/joy';
 import { Upload } from '@shared-data';
+import Grid from '@mui/material/Grid';
 
 export interface UploadCardProps {
   upload: Upload;
 }
 
 export const UploadCard = ({ upload }: UploadCardProps) => (
-  <Card>
-    <Card.Body>
-      {upload.userFirstName && upload.userLastName ? (
-        <span>
-          {upload.userFirstName} {upload.userLastName}{' '}
-        </span>
-      ) : (
-        <>Anonymous User {'  '}</>
-      )}
-      <span>{upload.date.toISOString().split('T').join(' ')}</span>
+  <Card style={{ marginBottom: '3%' }}>
+    <Grid container alignItems={'flex-end'} spacing={2}>
+      <Grid item>
+        {upload.userFirstName && upload.userLastName ? (
+          <Typography level={'h4'}>
+            {upload.userFirstName} {upload.userLastName}
+          </Typography>
+        ) : (
+          'Anonymous User'
+        )}
+      </Grid>
+      <Grid item>
+        <Typography level={'body-md'} style={{ paddingBottom: '1px' }}>
+          {upload.date.toDateString()}
+        </Typography>
+      </Grid>
+    </Grid>
 
-      <Table striped border={1}>
-        <thead>
-          <tr>
-            <th>Activity</th>
-            <th>Duration</th>
+    <Table size={'lg'} variant={'outlined'} borderAxis={'bothBetween'}>
+      <tbody>
+        {Object.entries(upload.workouts).map(([key, value]) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{value}</td>
           </tr>
-        </thead>
-        <tbody>
-          {Object.entries(upload.workouts).map(([key, value]) => (
-            <tr key={key}>
-              <td>{key}</td>
-              <td>{value}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Card.Body>
+        ))}
+      </tbody>
+    </Table>
   </Card>
 );
