e5d8477d9138a659b031a96ba3d1e8f413f74645
Conor <conor@johngregg.org>
2023-12-29 11:35:59 -0600

    NFF-19 Styling - Login uses mui

--- src/app/auth-pages/login/login.tsx
+++ src/app/auth-pages/login/login.tsx
@@ -1,12 +1,14 @@
 import { ChangeEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { Button, FloatingLabel, Form } from 'react-bootstrap';
 import { auth, createUserFromAuth, readUser } from '@shared-data';
 import { toast } from 'react-toastify';
+import { Button, Typography } from '@mui/joy';
+import TextField from '@mui/material/TextField';
 
 export const Login = () => {
   const navigate = useNavigate();
+
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
@@ -45,31 +47,39 @@ export const Login = () => {
   };
 
   return (
-    <>
-      <h1>Login</h1>
-      <Form onSubmit={handleSubmit}>
-        <FloatingLabel label={'Email'} className={'mb-2'}>
-          <Form.Control
-            required
-            type={'email'}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder={'Email'}
-          />
-        </FloatingLabel>
+    <form
+      onSubmit={handleSubmit}
+      autoComplete={'off'}
+      style={{ maxWidth: '500px' }}
+    >
+      <Typography level={'h2'} sx={{ mb: 3 }}>
+        Login
+      </Typography>
 
-        <FloatingLabel label={'Password'} className={'mb-2'}>
-          <Form.Control
-            required
-            type={'password'}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder={'Password'}
-          />
-        </FloatingLabel>
+      <TextField
+        label={'Email'}
+        type={'email'}
+        fullWidth
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        sx={{ mb: 3 }}
+      />
+
+      <TextField
+        label={'Password'}
+        type={'password'}
+        fullWidth
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        sx={{ mb: 3 }}
+      />
+
+      <div>
         <Button type={'submit'}>Login</Button>
-      </Form>
-    </>
+      </div>
+    </form>
   );
 };
 
