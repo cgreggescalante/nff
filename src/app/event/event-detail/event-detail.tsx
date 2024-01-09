433bdf387b80a36a4e944a53d96c752618a6b81b
Conor <conor@johngregg.org>
2024-01-09 10:56:46 -0600

    NFF-20 Goals - User can add goal when registering

--- src/app/event/event-detail/event-detail.tsx
+++ src/app/event/event-detail/event-detail.tsx
@@ -1,12 +1,27 @@
 import { Link, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { auth, CheckIsEventOwner, registerUserForEvent } from '@shared-data';
+import {
+  auth,
+  CheckIsEventOwner,
+  EventWithMetadata,
+  registerUserForEvent,
+} from '@shared-data';
 import { LoadingWrapper } from '@shared-ui';
 import { EventLeaderboard } from './event-leaderboard';
 import { TeamLeaderboard } from './team-leaderboard';
 import useAuth from '../../../providers/useAuth';
 import { useEvent } from '../../../providers/queries';
-import { Typography, Button } from '@mui/joy';
+import {
+  Button,
+  DialogContent,
+  DialogTitle,
+  Input,
+  Modal,
+  ModalClose,
+  ModalDialog,
+  Typography,
+} from '@mui/joy';
+import { toast } from 'react-toastify';
 
 export const EventDetail = () => {
   const { eventId } = useParams();
@@ -46,37 +61,85 @@ export const EventDetail = () => {
           <Typography level={'body-md'}>
             {event.entryRefs.length} Registered Users
           </Typography>
-          {event.registrationStart > new Date() ? (
-            <>Registration opens {event.registrationStart.toDateString()} </>
-          ) : event.registrationEnd > new Date() ? (
-            <>
-              {auth.currentUser !== null ? (
-                event.entryRefs.some((entryRef) =>
-                  entryRef.path.startsWith(`users/${auth.currentUser?.uid}`)
-                ) ? (
-                  <p>You have already registered for this event</p>
-                ) : (
-                  <Button
-                    onClick={() =>
-                      registerUserForEvent(event, auth.currentUser!.uid)
-                    }
-                  >
-                    Register Now
-                  </Button>
-                )
-              ) : (
-                <p>Please login to register</p>
-              )}
-              Registration ends {event.registrationEnd.toDateString()}
-            </>
-          ) : (
-            <>Registration is closed for this event</>
-          )}
+          <RegisterButton event={event} />
           <EventLeaderboard event={event} />
           <TeamLeaderboard event={event} />
         </>
       )}
     </LoadingWrapper>
+  );
+};
+
+const RegisterButton = ({ event }: { event: EventWithMetadata }) => {
+  if (event.registrationStart > new Date())
+    return (
+      <Typography level={'body-md'}>
+        Registration opens {event.registrationStart.toDateString()}
+      </Typography>
+    );
+
+  if (event.registrationEnd < new Date())
+    return (
+      <Typography level={'body-md'}>
+        Registration is closed for this event
+      </Typography>
+    );
+
+  if (auth.currentUser === null)
+    return <Typography level={'body-md'}>Please login to register</Typography>;
+
+  if (
+    event.entryRefs.some((entryRef) =>
+      entryRef.path.startsWith(`users/${auth.currentUser?.uid}`)
+    )
+  )
+    return (
+      <Typography level={'body-md'}>
+        You are already registered for this event
+      </Typography>
+    );
+
+  if (event.useGoals) return <RegisterForEventWithGoals event={event} />;
+
+  return (
+    <Button onClick={() => registerUserForEvent(event, auth.currentUser!.uid)}>
+      Register Now
+    </Button>
+  );
+};
+
+const RegisterForEventWithGoals = ({ event }: { event: EventWithMetadata }) => {
+  const [open, setOpen] = useState(false);
+
+  const [goal, setGoal] = useState<number>(0);
+
+  const register = () => {
+    registerUserForEvent(event, auth.currentUser!.uid, goal)
+      .then(() => setOpen(false))
+      .catch((error) => {
+        toast.error('Could not register for event');
+      });
+  };
+
+  return (
+    <>
+      <Button onClick={() => setOpen(true)}>Register Now</Button>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <ModalDialog>
+          <ModalClose />
+          <DialogTitle>Register for Event</DialogTitle>
+          <DialogContent>Please set a realistic goal.</DialogContent>
+          <Input
+            autoFocus
+            required
+            type={'number'}
+            value={goal}
+            onChange={(e) => setGoal(parseFloat(e.target.value))}
+          />
+          <Button onClick={register}>Register</Button>
+        </ModalDialog>
+      </Modal>
+    </>
   );
 };
 
